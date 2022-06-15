5df6937aaa822aa1a17ada863331034e7bafc98c
Ayan Mandal <ayan1mandal@gmail.com>
2022-06-15 20:49:11 +0100

    Updated config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://apollo-odyssey-demo.herokuapp.com/',
+  // uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
